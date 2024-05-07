03cf578f6b8081b9aa7997c3c9bb1540abca5cb3
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-05-07 16:02:02 -0700

    eslint updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     quotes: ['error', 'single'],
+    'eol-last': ['error', 'always'],
+    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+    'keyword-spacing': ['error', { 'before': true }],
     'no-undef': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-var-requires': 'off',
