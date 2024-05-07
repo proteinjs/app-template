6206f7a65aad6a9f75a2878bce02a29ebf057c83
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2024-05-07 16:40:35 -0700

    prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   root: true,
   ignorePatterns: ['**/dist/*', '**/node_modules/*'],
   rules: {
+    'prettier/prettier': ['error'],
     indent: ['error', 2],
     quotes: ['error', 'single'],
     'eol-last': ['error', 'always'],
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
-    'keyword-spacing': ['error', { 'before': true }],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'keyword-spacing': ['error', { before: true }],
     'no-undef': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
